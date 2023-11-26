081e9d93feff3143564068ca1d5a670dda3cb0a7
aquapi <aquaplmc@gmail.com>
2023-11-26 16:21:29 +0700

    Better scanDir

--- src/utils/scanDir.ts
+++ src/utils/scanDir.ts
@@ -2,89 +2,92 @@ import { join, relative } from 'path';
 import { Routes, type App } from '..';
 import { Glob } from 'bun';
 import { BaseConfig } from '../types/config';
-import { readdirSync, statSync, existsSync } from 'fs';
-
-const importRoute = async (
-    absPath: string,
-    app: App
-) => {
-    // Log the entry file
-
-    let fn = await import(absPath);
-
-    // Run the main function
-    if (!('main' in fn))
-        throw new Error(`Route file ${absPath} does not export a main function.`);
-
-    // Evaluate the main function
-    fn = fn.main(app);
-    if (fn instanceof Promise) fn = await fn;
-
-    // If it is not route throw an error
-    if (!(fn instanceof Routes))
-        throw new Error(`Route file ${absPath} main function does not return a routes group.`);
-
-    return fn as Routes<any>;
-}
+import { readdirSync, existsSync } from 'fs';
 
 const
-    // More patterns might be added
+    // Import a routes file
+    importRoute = async (
+        absPath: string,
+        app: App
+    ) => {
+        // Log the entry file
+        let fn = await import(absPath);
+
+        // Run the main function
+        if (!('main' in fn))
+            throw new Error(`Route file ${absPath} does not export a main function.`);
+
+        // Evaluate the main function
+        fn = fn.main(app);
+        if (fn instanceof Promise) fn = await fn;
+
+        // If it is not route throw an error
+        if (!(fn instanceof Routes))
+            throw new Error(`Route file ${absPath} main function does not return a routes group.`);
+
+        return fn as Routes<any>;
+    },
+
+    isFile = (path: string) =>
+        Bun.file(path).exists(),
+
+    // Import a config file 
+    importConfig = async (path: string) => {
+        if (existsSync(path)) {
+            let t = await import(path);
+            if (t.default) t = t.default;
+
+            return t as BaseConfig;
+        }
+
+        return null;
+    },
+
     routesPattern = new Glob('*.routes.*'),
 
-    f = async (
+    scan = async (
         directory: string,
         app: App,
         prefix: string = '/'
     ) => {
-
         // Check config
-        const config = join(directory, app.options.config), guards = [];
+        const config = await importConfig(
+            join(directory, app.options.config)
+        ), guards = [];
 
-        if (existsSync(config)) {
-            let baseConfig = await import(config) as BaseConfig;
+        if (config) {
+            // Add prefix
+            if (config.prefix) {
+                prefix = join(prefix, config.prefix);
 
-            // Load default export
-            if (baseConfig.default)
-                baseConfig = baseConfig.default as BaseConfig;
-
-            if (baseConfig) {
-                // Add prefix
-                if (baseConfig.prefix) {
-                    prefix = join(prefix, baseConfig.prefix);
-
-                    console.log(`- Current prefix: '${prefix}'`);
-                }
-
-                // Push guards
-                if (Array.isArray(baseConfig.guards))
-                    guards.push(...baseConfig.guards);
+                console.log(`- Current prefix: '${prefix}'`);
             }
+
+            // Push guards
+            if (config.guards)
+                guards.push(...config.guards);
         }
 
         // Check all routes
         for (var item of readdirSync(directory)) {
-            var itemPath = join(directory, item),
-                fileStat = statSync(itemPath);
+            var itemPath = join(directory, item);
 
-            if (fileStat.isFile()) {
-                // Register routes
-                if (routesPattern.match(item)) {
-                    // Log routes file path
-                    console.info(`+ Entry: ${relative(directory, itemPath)}'`);
+            if (isFile(itemPath)) {
+                if (!routesPattern.match(item)) continue;
 
-                    var route = await importRoute(itemPath, app);
+                // Log routes file path
+                console.info(`+ Entry: ${relative(directory, itemPath)}'`);
 
-                    // Extend
-                    app.routes.extend(
-                        route.prependGuards(...guards).prefix(prefix)
-                    );
-                }
+                // Extend
+                app.routes.extend(
+                    // Get the route
+                    (await importRoute(itemPath, app))
+                        .prependGuards(...guards)
+                        .prefix(prefix)
+                );
             }
-
-            // Check directory
-            else if (fileStat.isDirectory())
-                await f(itemPath, app, prefix);
+            else await scan(itemPath, app, prefix);
         }
     }
 
-export default f;
+export default scan;

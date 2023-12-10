6e6ea08429836cc377107f90729e06c3135d25d4
aquapi <aquaplmc@gmail.com>
2023-12-10 10:01:50 +0700

    Fix a bug when checking file

--- tests/app/src/base.ts
+++ tests/app/src/base.ts
@@ -1,6 +1,8 @@
-import { config } from '@stricjs/app';
+import { config, layer } from '@stricjs/app';
 
-export default config.guard('/api', c => {
-    console.log('A request arrived.');
-    console.log('URL:', c.url);
-});
+export default config.guard(
+    '/api', layer(c => {
+        console.log('A request arrived.');
+        console.log('URL:', c.url);
+    })
+);
